8f3bf4f7b0214c05d8fa803a346cbc368252278d
Philip Plamenov Konqrov <philip_plamenov@yahoo.com>
2021-08-05 17:13:21 +0300

    Initial commit.

---
+++ Keyless/Controllers/NewController/DashboardCell.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardCell.swift
+//  Keyless
+//
+//  Created by Philip Plamenov on 23.06.20.
+//
+
+import UIKit
+
+class DashboardCell: UITableViewCell {
+
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
